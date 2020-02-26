1b5a41c0075fa393d4ea9fb8e1b8ae19cbd36825
Hunter <hmvitous@yahoo.com>
2020-02-26 15:20:36 +0100

    Fixes an error to make the input fields test pass

--- src/components/InputFields.jsx
+++ src/components/InputFields.jsx
@@ -4,16 +4,15 @@ const InputFields = ({ onChangeHandler }) => {
   return (
     <>
       <label>Distance</label>
-      <input
-        onChange={this.onChangeHandler} name="distance" id="distance"></input>
+      <input onChange={onChangeHandler} name="distance" id="distance"></input>
 
-      <select onChange={this.onChangeHandler} name="gender" id="gender">
+      <select onChange={onChangeHandler} name="gender" id="gender">
         <option value="female">Female</option>
         <option value="male">Male</option>
       </select>
 
       <label>Age</label>
-      <input onChange={this.onChangeHandler} name="age" id="age"></input>
+      <input onChange={onChangeHandler} name="age" id="age"></input>
     </>
   );
 };
