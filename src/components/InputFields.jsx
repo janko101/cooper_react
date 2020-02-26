fa7e3cf2804cff3a5023c1233614132c161145c0
Hunter <hmvitous@yahoo.com>
2020-02-26 15:17:08 +0100

    Refactors the input fields andputs them in their own file

---
+++ src/components/InputFields.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const InputFields = ({ onChangeHandler }) => {
+  return (
+    <>
+      <label>Distance</label>
+      <input
+        onChange={this.onChangeHandler} name="distance" id="distance"></input>
+
+      <select onChange={this.onChangeHandler} name="gender" id="gender">
+        <option value="female">Female</option>
+        <option value="male">Male</option>
+      </select>
+
+      <label>Age</label>
+      <input onChange={this.onChangeHandler} name="age" id="age"></input>
+    </>
+  );
+};
+
+export default InputFields;
