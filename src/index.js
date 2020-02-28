739a8660d1cb50f73d634ebdfff32e7c2962b0f5
Hunter <hmvitous@yahoo.com>
2020-02-28 12:13:34 +0100

    Edits the heroku url

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import axios from "axios";
 
-axios.defaults.baseURL = "https://coopertest.herokuapp.com/";
+axios.defaults.baseURL = "https://git.heroku.com/coopertest.git";
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
