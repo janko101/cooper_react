8a91174392551de7dea5e89ba86af5f8fafbff2e
Hunter <hmvitous@yahoo.com>
2020-02-29 13:18:29 +0100

    Changes the url for the back end

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import axios from "axios";
 
-axios.defaults.baseURL = "https://git.heroku.com/coopertest.git";
+axios.defaults.baseURL = "https://coopertest.herokuapp.com/api/v1";
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
